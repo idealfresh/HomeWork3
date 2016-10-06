ab14a49ee2cf8b367c6988c12ba96be4d2ef15fc
owner90 <owner90@192.168.5.3>
2016-10-06 13:38:21 +0300

    Две числа от тип int и отпечатване на тяхната сума

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Напишете програма, която чете от конзолата 2 числа от тип int и отпечатва тяхната сума.
+		
+		int x=1;
+				int y=2;
+			System.out.println(x+y);
+			
+	}
+
+}
